13838f0c2eff1b721145cbc4a6b907d98df0a442
Stas <sizovstanislav10@gmail.com>
2023-06-28 16:47:18 +0700

    m4 t2 v2

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,11 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        int a=0;
-        while (a<= chars.length){
-            System.out.print(chars);
-            a++;
-        }
+
     }
 
 }
