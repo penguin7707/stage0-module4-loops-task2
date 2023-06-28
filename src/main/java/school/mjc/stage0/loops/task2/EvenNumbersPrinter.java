f0a42d6c8e0829949e031c12936aa536064e88d5
Stas <sizovstanislav10@gmail.com>
2023-06-28 16:51:31 +0700

    m4 t2 v2

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
+        int first =0;
+        while (first<=printTillInclusive){
+            if (first==0 || first%2==0) System.out.println(first);
+            first++;
+        }
 
     }
 }
